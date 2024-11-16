507fef3e776a15f50bd2fd053fa19757c520e886
Rahul G <rahulg.github@gmail.com>
2024-11-16 14:38:09 +0530

    autorun updated

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -3,7 +3,7 @@
 alttab -w 1 -fg '#aabac8' -bg '#000000' -frame '#cd00cd' -t 128x150 -i 127x64 -d 1 &
 dunst &
 picom &
-compton &
 nm-applet &
+xclickroot xmenu_run &
 xset s 540
 xset dpms 600 600 600
